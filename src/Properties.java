5c83f4d4ff51f2f709a9fa9cc221be493c93ec05
Imran Latif <wolf2184@gmail.com>
2021-10-16 13:44:05 -0400

    Finished Properties.java

--- src/Properties.java
+++ src/Properties.java
@@ -1,5 +1,27 @@
 public class Properties {
     private String name;
     private int[] rent; //To account for houses / hotels
+    private Player owner;
 
+    public Properties( String name, int[] rentArray, Player owner){
+        this.name = name;
+        this.rent = rentArray;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getRent() {
+        return rent;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
 }
