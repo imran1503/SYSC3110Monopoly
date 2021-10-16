90d21ba593be5c249f80f7003dfb6d68571869a3
Imran Latif <wolf2184@gmail.com>
2021-10-16 17:13:59 -0400

    Fixed Constructor

--- src/GO.java
+++ src/GO.java
@@ -1,6 +1,8 @@
+import java.awt.*;
+
 public class GO extends Properties{
-    public GO(String name, int rent, Player owner, int price) {
-        super(name, 0, null, 0);
+    public GO(String name, int rent, Player owner, int price, Color color) {
+        super(name, 0, null, 0, color);
     }
 
     public void giveMoney(Player player) {
