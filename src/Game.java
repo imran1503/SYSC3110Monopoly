bb3a50d975648cf92c3537efd33b2cb6ce1621ca
Imran Latif <wolf2184@gmail.com>
2021-10-17 19:52:33 -0400

    Started Game

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.Enumeration;
+
 public class Game {
+    private ArrayList<Player> players;
+    private ArrayList<Properties> propertiesArrayList;
+    private Enumeration gameState; //todo roll & move, purchase, house/hotel, end turn.
+
+    public Game( ){
+        this.players = new ArrayList<Player>();
+        this.propertiesArrayList = new ArrayList<Properties>();
+
+    }
+
+
+
 }
