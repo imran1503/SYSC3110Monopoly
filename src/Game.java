42bd864c779624be5b5c1a13be01f5ca4b63e76c
muhammadfurqan <muhammadfurqan@cmail.carleton.ca>
2021-10-25 16:36:05 -0400

    game.java improvements in number of getter method calls..

--- src/Game.java
+++ src/Game.java
@@ -164,6 +164,7 @@ public class Game {
      * @return boolean true if game still in progress, false if not.
      */
     private boolean operateCommand(String command) {
+        String playerName = currentPlayer.getName();
         if (command.equals("quit")) {
             System.out.println("Game has ended.");
             return false;
@@ -174,20 +175,20 @@ public class Game {
             }
             else{
 
-                System.out.println(currentPlayer.getName()+" can NOT roll again. Pass your turn or buy property.");
+                System.out.println(playerName+" can NOT roll again. Pass your turn or buy property.");
             }
             if ((currentPlayer.getInJail() == true)){
                 Boolean isDouble = roll();
                 if (isDouble && (currentPlayer.getTurnsInJail() != 0)) {
                     currentPlayer.setInJail(false);
                     currentPlayer.setTurnsInJail(0);
-                    System.out.println(currentPlayer.getName() + " rolled a double and is out of jail.");
+                    System.out.println(playerName + " rolled a double and is out of jail.");
                 } else {
                     if (currentPlayer.getTurnsInJail() == 3) {
                         currentPlayer.removefromBalance(50);
                         currentPlayer.setInJail(false);
                         currentPlayer.setTurnsInJail(0);
-                        System.out.println(currentPlayer.getName() + " Payed $50 to get out of jail.");
+                        System.out.println(playerName + " Payed $50 to get out of jail.");
                     }
                     currentPlayer.setTurnsInJail(currentPlayer.getTurnsInJail() + 1); //add 1 to time in jail for player.
                     passPlayerTurn();
@@ -223,7 +224,7 @@ public class Game {
         else if (command.equals("pass turn")) {
 
             if (nextRoll == true) {
-                System.out.println(currentPlayer.getName() + " needs to roll again before passing turn. Rolled a double previously.");
+                System.out.println(playerName + " needs to roll again before passing turn. Rolled a double previously.");
             } else {
                 passPlayerTurn();
                 nextRoll = true;
@@ -321,26 +322,27 @@ public class Game {
         int randomRoll1 = ((int)((Math.random()*60)%6) + 1);
         int randomRoll2 = ((int)((Math.random()*77)%6) + 1);
         int playerPosition = currentPlayer.getPositon();
+        String playerName = currentPlayer.getName();
         if(((playerPosition + randomRoll1 + randomRoll2) == 30)&&!currentPlayer.getInJail()){
             currentPlayer.setInJail(true); //Postion for Go to Jail on Board is 30 and Jail is 10.
             currentPlayer.setPosition(10);
-            System.out.println(currentPlayer.getName()+" has been set to Jail, roll a double to get out of Jail next turn.");
+            System.out.println(playerName+" has been set to Jail, roll a double to get out of Jail next turn.");
         }
         if(!currentPlayer.getInJail()) {
             if ((playerPosition + randomRoll1 + randomRoll2) >= totalNumOfSpaces) {
                 currentPlayer.setPosition((randomRoll1 + randomRoll2 + playerPosition) - totalNumOfSpaces);
                 currentPlayer.addToBalance(200); //Passing Go
-                System.out.println(currentPlayer.getName()+" has passed Go, Balance is now "+currentPlayer.getBalance());
+                System.out.println(playerName+" has passed Go, Balance is now "+currentPlayer.getBalance());
             } else {
                 currentPlayer.setPosition(randomRoll1 + randomRoll2 + playerPosition);
             }
         }
         if(randomRoll1 == randomRoll2){
             System.out.println("You rolled a double, you can roll again.");
-            //todo tell player where they landed with their first roll before they roll again
+            System.out.println(playerName+" rolled a "+(randomRoll1+randomRoll2)+ ", landed on "+propertiesArrayList.get(currentPlayer.getPositon()).getName());
             return true;
         }
-        System.out.println(currentPlayer.getName()+" rolled a "+(randomRoll1+randomRoll2)+ ", landed on "+propertiesArrayList.get(currentPlayer.getPositon()).getName());
+        System.out.println(playerName+" rolled a "+(randomRoll1+randomRoll2)+ ", landed on "+propertiesArrayList.get(currentPlayer.getPositon()).getName());
         return false;
     }
 
@@ -351,23 +353,25 @@ public class Game {
     public void purchaseProperty(){
         int playerPosition = currentPlayer.getPositon();
         Properties landedOnProperty = propertiesArrayList.get(playerPosition);
+        String propertyName = landedOnProperty.getName();
+        String playerName = currentPlayer.getName();
         if(landedOnProperty.getPrice() == 0){
-            System.out.println("This property can not be purchased, Property Name: "+landedOnProperty.getName());
+            System.out.println("This property can not be purchased, Property Name: "+propertyName);
         }
         else{ if(landedOnProperty.getOwner().equals(currentPlayer)){
-            System.out.println("This property belongs to you already, Property Name: "+landedOnProperty.getName());
+            System.out.println("This property belongs to you already, Property Name: "+propertyName);
         }
         else{ if(!landedOnProperty.getOwner().getName().equals("bank")){
-            System.out.println("This property belongs to someone else, Property Name: "+landedOnProperty.getName());
+            System.out.println("This property belongs to someone else, Property Name: "+propertyName);
         }
         else{ if(currentPlayer.getBalance() < landedOnProperty.getPrice()){
-            System.out.println(currentPlayer.getName()+" does Not have enough money to purchase this house/hotel, Property Name: "+landedOnProperty.getName());
+            System.out.println(playerName+" does Not have enough money to purchase this house/hotel, Property Name: "+propertyName);
         }
         else {
             currentPlayer.removefromBalance(landedOnProperty.getPrice());
             landedOnProperty.setOwner(currentPlayer);
             currentPlayer.gainTerritory(landedOnProperty);
-            System.out.println(currentPlayer.getName() + " purchased "+landedOnProperty.getName()+", remaining Balance:"+currentPlayer.getBalance());
+            System.out.println(playerName + " purchased "+propertyName+", remaining Balance:"+currentPlayer.getBalance());
         }}}}
     }
 
@@ -380,36 +384,39 @@ public class Game {
         Boolean owningEqualHouses = true;
         int numOfHouseCurrent = property.getNumHouses();
         Color colorOfProperty = property.getColor();
+        String propertyName = property.getName();
+        String playerName = currentPlayer.getName();
         int sizeOfColorSet = colorPropertiesArrayList.get(colorOfProperty).size();
         for(int i = 0; i < sizeOfColorSet; i++){
-            if(!colorPropertiesArrayList.get(colorOfProperty).get(i).getOwner().equals(currentPlayer)){
+            Properties propertySameColor = colorPropertiesArrayList.get(colorOfProperty).get(i);
+            if(!propertySameColor.getOwner().equals(currentPlayer)){
                 owningColorSet = false;
             }
-            if(!((colorPropertiesArrayList.get(colorOfProperty).get(i).getNumHouses() >= numOfHouseCurrent)||(colorPropertiesArrayList.get(colorOfProperty).get(i).getNumHotels() == 1))){
+            if(!((propertySameColor.getNumHouses() >= numOfHouseCurrent)||(propertySameColor.getNumHotels() == 1))){
                 owningEqualHouses = false;
             }
         }
         if(!owningColorSet){
-            System.out.println(currentPlayer.getName()+" does NOT own the color set of this property, Property Name: "+property.getName());
+            System.out.println(playerName+" does NOT own the color set of this property, Property Name: "+propertyName);
         }
-        else{ if(!owningEqualHouses){
-            System.out.println(currentPlayer.getName()+" does NOT own enough of houses for the color set to buy more, Property Name: "+property.getName());
+        else if(!owningEqualHouses){
+            System.out.println(playerName+" does NOT own enough of houses for the color set to buy more, Property Name: "+propertyName);
         }
-        else{ if(currentPlayer.getBalance() < property.getHousePrice()){
-            System.out.println(currentPlayer.getName()+" does Not have enough money to purchase this property, Property Name: "+property.getName());
+        else if(currentPlayer.getBalance() < property.getHousePrice()){
+            System.out.println(playerName+" does Not have enough money to purchase this property, Property Name: "+propertyName);
         }
-        else{if(property.getNumHotels() == 1){
-            System.out.println("This property already has a hotel, property Name: "+property.getName());
-        }
-        else{ if(numOfHouseCurrent == 4) {
-            currentPlayer.removefromBalance(property.getHousePrice());
-            property.setNumHotels(1);
-            property.setNumHouses(0);
+        else if(property.getNumHotels() == 1){
+            System.out.println("This property already has a hotel, property Name: "+propertyName);
         }
         else{
             currentPlayer.removefromBalance(property.getHousePrice());
+            if(numOfHouseCurrent == 4) {
+            property.setNumHotels(1);
+            property.setNumHouses(0);
+            }
+            else{
             property.setNumHouses((1+numOfHouseCurrent));
-        }}}}}
+        }}
     }
 
     /**
