1f454c07e956ec2435eff08654378e09039d954d
MahtabAmeli <Adminjan93>
2021-11-05 13:56:41 -0400

    Corrected some comments in BoardView.

--- src/BoardView.java
+++ src/BoardView.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.concurrent.Flow;
-
 
 /**
  * class BoardView implements the graphical user interface's view for the board game Monopoly.
@@ -96,71 +94,54 @@ public class BoardView {
      */
     private void createPropertyPanels() {
         // Create 40 panels, one for each property
-        // and one for the large centre panel which does not include properties
         this.propertyPanels = new JPanel[40];
 
-        //Create 41 JLabels, one for each propertyPanel
+        //Create 40 JLabels, one for each propertyPanel
         JLabel [] propertyIndexLabels = new JLabel[40];
 
         //todo: the code is heavily duplicated. Will fix later.
-        for (int i = 0; i < 40;i++) {
-
+        for (int i = 0; i < 40; i++) {
             // Properties 0 to 10 are added to the south panel
             if (i < 11) {
                 propertyPanels[i] = new JPanel(new BorderLayout());
-                // todo fix later
                 //setting a different background color for each propertyPanel to visually set them apart from each other for now
                 propertyPanels[i].setBackground(new Color(255 - (4*i), 255 - (4*i), 255 - (4*i)));
-
                 //creating and adding an index label to each propertyPanel to see their order
                 propertyIndexLabels[i] = new JLabel();
                 propertyIndexLabels[i].setText("" + i);
                 propertyPanels[i].add(propertyIndexLabels[i]);
                 south.add(propertyPanels[i]);
-
             }
             // Properties 11 to 19 are added to the west panel.
             else if (i < 20) {
                 propertyPanels[i] = new JPanel(new BorderLayout());
-                // todo fix later
                 //setting a different background color for each propertyPanel to visually set them apart from each other for now
                 propertyPanels[i].setBackground(new Color(255 - (4*i), 255 - (4*i), 255 - (4*i)));
-
-                //creating and adding an index label to each propertyPanel to see their order
                 propertyIndexLabels[i] = new JLabel();
                 propertyIndexLabels[i].setText("" + i);
                 propertyPanels[i].add(propertyIndexLabels[i]);
                 west.add(propertyPanels[i]);
-
             }
             // Properties 20 to 30 are added to the north panel.
             else if (i < 31) {
                 propertyPanels[i] = new JPanel(new BorderLayout());
-                // todo fix later
                 //setting a different background color for each propertyPanel to visually set them apart from each other for now
                 propertyPanels[i].setBackground(new Color(255 - (4*i), 255 - (4*i), 255 - (4*i)));
-
-                //creating and adding an index label to each propertyPanel to see their order
                 propertyIndexLabels[i] = new JLabel();
                 propertyIndexLabels[i].setText("" + i);
                 propertyPanels[i].add(propertyIndexLabels[i]);
                 north.add(propertyPanels[i]);
-
             }
             // Properties 31 to 39 are added to the east panel.
             else {
                 propertyPanels[i] = new JPanel(new BorderLayout());
-                // todo fix later
                 //setting a different background color for each propertyPanel to visually set them apart from each other for now
                 propertyPanels[i].setBackground(new Color(255 - (4*i), 255 - (4*i), 255 - (4*i)));
-
-                //creating and adding an index label to each propertyPanel to see their order
                 propertyIndexLabels[i] = new JLabel();
                 propertyIndexLabels[i].setText("" + i);
                 propertyPanels[i].add(propertyIndexLabels[i]);
                 east.add(propertyPanels[i]);
             }
-
         }
     }
 
@@ -168,17 +149,14 @@ public class BoardView {
      *
      */
     private void createGamePanel() {
-        // gamePanel contains the board's properties
-
         gamePanel = new JPanel(new BorderLayout());
-
 
         //todo fix size so that gamePanel is always a square with same side length as
         // controlPanel's height
         gamePanel.setPreferredSize(new Dimension(800, 800));
         gamePanel.setBackground(new Color(255, 255, 255));
 
-        //gamePanel is made oof three panels: top, center, and bottom
+        //gamePanel is made of 5  other panels: north, east, west, south, center
         north = new JPanel(new GridLayout(1,11));
         east = new JPanel(new GridLayout(9,1));
         west = new JPanel(new GridLayout(9,1));
@@ -188,7 +166,6 @@ public class BoardView {
         north.setBackground(new Color(90,20,200));
         north.setPreferredSize(new Dimension(800,150));
         north.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-
 
         east.setBackground(new Color(100,140,10));
         east.setPreferredSize(new Dimension(150, 500));
@@ -204,9 +181,9 @@ public class BoardView {
         south.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
 
         center.setBackground(new Color(190,250,250));
+        // Roll Dice button appears in center of the board
         rollButton = new JButton("Roll Dice");
         center.add(rollButton);
-
 
         gamePanel.add(north, BorderLayout.NORTH);
         gamePanel.add(east, BorderLayout.EAST);
@@ -215,9 +192,7 @@ public class BoardView {
         gamePanel.add(center, BorderLayout.CENTER);
 
         createPropertyPanels();
-
     }
-
 
     /** creates controlPanel, which contains all controls and buttons
      * including purchase property, and quit game
@@ -226,14 +201,13 @@ public class BoardView {
      *
      */
     private void createControlPanel() {
-        // controlPanel contains the buttons for purchasing property, and quiting the game.
+        // controlPanel contains the buttons for starting a new game, purchasing property, and quiting the game.
         // The message windows that appear as a result of pressing these buttons will also appear on eastPanel
         controlPanel = new JPanel(new FlowLayout());
         //todo fix size so that controlPanel in narrower than gamePanel but has same height. size currently incorrect.
         controlPanel.setPreferredSize(new Dimension(300, 800));
         controlPanel.setBackground(new Color(215, 200, 131, 255));
 
-        // buttons for New Game, purchase Property, and Quit Game
         // todo add more buttons later
         newGameButton = new JButton("New Game");
         purchaseButton = new JButton("Purchase Property");
@@ -243,8 +217,6 @@ public class BoardView {
         controlPanel.add(newGameButton);
         controlPanel.add(purchaseButton);
         controlPanel.add(quitButton);
-
-
     }
 
 
@@ -272,9 +244,7 @@ public class BoardView {
      * displays the GUI of the game.
      */
     public void displayGUI(){
-        // todo set size so that the board is always a square and
-        //  arrangement of propertyPanels remain the same regardless of size
-        frame.setSize(new Dimension(1100, 800));   //set size of the main frame
+        frame.setSize(new Dimension(1100, 800));
         closeFrame();
     }
 
