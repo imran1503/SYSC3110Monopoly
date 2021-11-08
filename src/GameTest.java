97241e61f1a86d2d4ef8d57329800563531c7ac9
MahtabAmeli <Adminjan93>
2021-11-08 17:06:25 -0500

    Added more tests to GameTest.

--- src/GameTest.java
+++ src/GameTest.java
@@ -84,15 +84,53 @@ public class GameTest {
     }
 
 
+    // testing if player moves forward on the board after rolling
     @Test
     public void testRollCase1(){
         game = new Game();
         p1 = new Player("P1", new Color(10,10,10),1500);
+        p1.setInJail(false);
         game.addPlayer(p1);
         game.setCurrentPlayer(p1);
+        //game.set
+    }
 
-        game.roll();
-        assertNotEquals( 0,p1.getPositon());
+    //test case for when player is not in jail and rolls a double. roll() must return true.
+    @Test
+    public void testRollCase2(){
+        game = new Game();
+        p1 = new Player("P1", new Color(10,10,10),1500);
+        game.setCurrentPlayer(p1);
+        p1.setInJail(false);
+        game.addPlayer(p1);
+
+        game.setDiceValue1(2);
+        game.setDiceValue2(2);
+
+        Boolean actualValue = game.roll();
+        while(game.getDiceValue1() != game.getDiceValue2()){
+            actualValue = game.roll();
+        }
+        assertEquals( true, actualValue);
+    }
+
+    //test case for when player is in jail and rolls a double. roll() must return false.
+    @Test
+    public void testRollCase3(){
+        game = new Game();
+        p1 = new Player("P1", new Color(10,10,10),1500);
+        game.setCurrentPlayer(p1);
+        p1.setInJail(true);
+        game.addPlayer(p1);
+
+        game.setDiceValue1(2);
+        game.setDiceValue2(2);
+
+        Boolean actualValue = game.roll();
+        while(game.getDiceValue1() != game.getDiceValue2()){
+            actualValue = game.roll();
+        }
+        assertEquals( false, actualValue);
     }
 
     @Test
@@ -121,5 +159,16 @@ public class GameTest {
     }
 
 
+    @Test
+    public void testPurchaseProperty(){
+        game = new Game();
+        p1 = new Player("P1", new Color(10,10,10),1500);
+        game.setCurrentPlayer(p1);
+        p1.setPosition(6);
+        game.operateCommand(Game.Commands.purchaseProperty);
+        Boolean actualValue = (p1.getControlledProperties().get(0).getName().equals("Oriental Avenue")) &&(p1.getBalance() == 1400);
+        assertEquals(true,actualValue);
+
+    }
 
 }
