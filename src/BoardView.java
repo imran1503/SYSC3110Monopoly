02ec418759b3ed6562f785ca075004fa3f17c139
muhammadfurqan <muhammadfurqan@cmail.carleton.ca>
2021-12-06 17:13:43 -0500

    housing icons fix

--- src/BoardView.java
+++ src/BoardView.java
@@ -483,12 +483,18 @@ public class BoardView {
                 housing5Labels[propertyLocation].setVisible(false);
             }
             else if(propertyNumHouses == 2){
+                housing1Labels[propertyLocation].setVisible(true);
                 housing2Labels[propertyLocation].setVisible(true);
             }
             else if(propertyNumHouses == 3){
+                housing1Labels[propertyLocation].setVisible(true);
+                housing2Labels[propertyLocation].setVisible(true);
                 housing3Labels[propertyLocation].setVisible(true);
             }
             else if(propertyNumHouses == 4){
+                housing1Labels[propertyLocation].setVisible(true);
+                housing2Labels[propertyLocation].setVisible(true);
+                housing3Labels[propertyLocation].setVisible(true);
                 housing4Labels[propertyLocation].setVisible(true);
             }
         }
